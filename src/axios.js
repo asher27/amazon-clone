58620f9693f49f46186e0dc70bbec3c735d7345b
David Hwang <34634220+asher27@users.noreply.github.com>
2022-12-28 23:41:46 +0900

    Delete axios.js

--- src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const instance = axios.create({
-    // baseURL: 'http://127.0.0.1:5001/clone-767d1/us-central1/api', // API Url
-    baseURL: 'https://us-central1-clone-767d1.cloudfunctions.net/api', // API Url
-})
-
-export default instance
